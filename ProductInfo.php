96cb60b43f9e8ed519faa869b7e20cdbef083fb0
shuai.liu.sz <shuai.liu.sz@tcl.com>
2015-07-03 18:51:49 +0800

    add code for more Product Info

--- ProductInfo.php
+++ ProductInfo.php
@@ -40,7 +40,7 @@ class ProductInfo {
     }
     
     public  function setCurrentDate($date) {
-        $this->currentDate = $date;
+        $this->currentDate = date("Y-m-d", strtotime($date));
     }
     
     public function getProductInfo($db) {
@@ -52,24 +52,42 @@ class ProductInfo {
             $this->count = round(($this->cost / $arr[0]['net_worth']), 2);
         }
         //########################################################################
-        $currentDate = date("Y-m-d", strtotime("-1 day", strtotime($this->currentDate)));
+//        $currentDate = date("Y-m-d", strtotime("-1 day", strtotime($this->currentDate)));
+        $currentDate = $this->currentDate;
         
-        $sql_query = "SELECT * FROM `product_detail` WHERE code='8193' AND date<='$currentDate'";
+        $sql_query = "SELECT * FROM `product_detail` WHERE code='8193' AND date>='$this->date' AND date<'$currentDate'";
         $query_result = $db->query($sql_query);
         $arr = $db->fetchAll();
         $arr_count = count($arr, COUNT_NORMAL);
         if ($arr_count != 0) {
             $this->assets = round($arr[0]['net_worth'] * $this->count, 2);
             $this->yields = $this->assets - $this->cost;
+            $this->yields = round($this->yields, 2);
+            if ($arr_count > 1 && self::isWeekday($currentDate) && self::getLastWeekday($currentDate) == $arr[0]['date']) {
+                $lastAssets = round($arr[1]['net_worth'] * $this->count, 2);
+                $this->dayYield = round($this->assets - $lastAssets, 2);
+            }
         }
-        //########################################################################
-        $lastDay = date("Y-m-d", strtotime("-1 day", strtotime($currentDate)));
-        $dayYieldRate = self::getYieldRate($db, $currentDate, $lastDay);
-        $this->dayYield = $dayYieldRate * $this->count;
         //########################################################################
         $lastWeek = date("Y-m-d", strtotime("-1 week", strtotime($currentDate)));
         $weekYieldRate = (self::getYieldRate($db, $currentDate, $lastWeek) * $this->count * 365) / $this->cost;
         $this->weekYieldRate = $weekYieldRate;
+        $this->weekYieldRate = round($this->weekYieldRate, 4);
+        //########################################################################
+        $lastMonth = date("Y-m-d", strtotime("-1 month", strtotime($currentDate)));
+        $monthYieldRate = (self::getYieldRate($db, $currentDate, $lastMonth) * $this->count * 365) / $this->cost;
+        $this->monthYieldRate = $monthYieldRate;
+        $this->monthYieldRate = round($this->monthYieldRate, 4);
+        //########################################################################
+        $lastQuarter = date("Y-m-d", strtotime("-3 month", strtotime($currentDate)));
+        $quarterYieldRate = (self::getYieldRate($db, $currentDate, $lastQuarter) * $this->count * 365) / $this->cost;
+        $this->quarterYieldRate = $quarterYieldRate;
+        $this->quarterYieldRate = round($this->quarterYieldRate, 4);
+        //########################################################################
+        $lastYear = date("Y-m-d", strtotime("-1 year", strtotime($currentDate)));
+        $yearYieldRate = (self::getYieldRate($db, $currentDate, $lastYear) * $this->count * 365) / $this->cost;
+        $this->yearYieldRate = $yearYieldRate;
+        $this->yearYieldRate = round($this->yearYieldRate, 4);
     }
     
     /*
@@ -77,7 +95,7 @@ class ProductInfo {
      * 结果 * 份数 * 365就是年收益，再除以成本就是这几天的年化收益率
      */
     public static function getYieldRate($db, $max_date, $min_date) {
-        $sql_query = "SELECT * FROM `product_detail` WHERE code='8193' AND date>='$min_date' AND date<='$max_date'";
+        $sql_query = "SELECT * FROM `product_detail` WHERE code='8193' AND date>='$min_date' AND date<'$max_date'";
         $query_result = $db->query($sql_query);
         $arr = $db->fetchAll();
         $arr_count = count($arr, COUNT_NORMAL);
@@ -103,6 +121,23 @@ class ProductInfo {
         return $yieldRate;
     }
     
+    public function isWeekday($date) {
+        $weekNum = date("w", strtotime($date));
+        if ($weekNum > 0 && $weekNum < 6) {
+            return true;
+        }
+        return false;
+    }
+    
+    public function getLastWeekday($date) {
+        $lastDateTimestamp = strtotime($date);
+        do {
+            $lastDateTimestamp = strtotime("-1 day", $lastDateTimestamp);
+            $lastDate = date("Y-m-d", $lastDateTimestamp);
+        } while(!self::isWeekday($lastDate));
+        return $lastDate;
+    }
+    
     public function showProductInfo() {
         echo '产品代码：' . $this->code . '</br>';
         echo '产品名称：' . $this->name . '</br>';
@@ -112,8 +147,11 @@ class ProductInfo {
         echo '当前日期：' . $this->currentDate . '</br>';
         echo '累计收益：' . $this->yields . '</br>';
         echo '总资产：' . $this->assets . '</br>';
-        echo '当日收益：' . $this->dayYield . '</br>';
+        echo '昨日收益：' . $this->dayYield . '</br>';
         echo '七日年化收益率：' . $this->weekYieldRate . '</br>';
+        echo '单月年化收益率：' . $this->monthYieldRate . '</br>';
+        echo '季度年化收益率：' . $this->quarterYieldRate . '</br>';
+        echo '年度年化收益率：' . $this->yearYieldRate . '</br>';
     }
 }
 
